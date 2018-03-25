6c0a282dddc4d16d341751decffa01f70527619b
Luke Chang <luke@csel.co.nz>
2018-03-26 09:08:12 +1300

    get started

---
+++ create-react-component-cli.js
@@ -0,0 +1,7 @@
+var ejs = require('ejs');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+var program = require('commander')
+var readline = require('readline')
+var util = require('util')
